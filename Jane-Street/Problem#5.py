e967e2ed7588a282f59df47670d08eb4aa0e8b9e
anantkaushik <anant.kaushik2@gmail.com>
2018-11-25 00:09:36 +0530

    Jane Street

---
+++ Jane-Street/Problem#5.py
@@ -0,0 +1,28 @@
+"""
+cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that pair. 
+For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
+
+Given this implementation of cons:
+def cons(a, b):
+    def pair(f):
+        return f(a, b)
+    return pair
+Implement car and cdr.
+"""
+def cons(a, b):
+    def pair(f):
+        return f(a, b)
+    return pair
+
+def car(f):
+    def left(a, b):
+        return a
+    return f(left)
+
+def cdr(f):
+    def right(a, b):
+        return b
+    return f(right)
+
+print(car(cons(3,4)))
+print(cdr(cons(3,4)))
