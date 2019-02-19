01d0cabebe63531a0c486bfd485d534ef54c9cdd
anantkaushik <anant.kaushik2@gmail.com>
2019-02-19 23:40:30 +0530

    Google

---
+++ Google/Problem#104.py
@@ -0,0 +1,17 @@
+"""
+Determine whether a doubly linked list is a palindrome. What if it’s singly linked?
+
+For example, 1 -> 4 -> 3 -> 4 -> 1 returns true while 1 -> 4 returns false.
+"""
+def isPalindromeSingly(head):
+    rev = None
+    slow = fast = head
+    while fast and fast.next:
+        fast = fast.next.next
+        rev, rev.next, slow = slow, rev, slow.next
+    if fast:
+        slow = slow.next
+    while rev and rev.data == slow.data:
+        rev = rev.next
+        slow = slow.next
+    return not rev
