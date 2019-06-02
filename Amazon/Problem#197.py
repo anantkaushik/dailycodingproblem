9fa6ee2cd54f445946713266f7ace9c4783d8e59
anantkaushik <anant.kaushik2@gmail.com>
2019-06-02 16:24:50 +0530

    Array Rotation

---
+++ Amazon/Problem#197.py
@@ -0,0 +1,17 @@
+"""
+Given an array and a number k that's smaller than the length of the array, rotate the array to the right k elements in-place.
+"""
+def rotate(self, nums, k):
+    k = k % len(nums)
+    count = start = 0
+    while count < len(nums):
+        cur = start
+        prev = nums[start]
+        while True:
+            nextIndex = (cur + k) %len(nums)
+            nums[nextIndex], prev = prev, nums[nextIndex]
+            cur = nextIndex
+            count += 1
+            if start == cur:
+                break
+        start += 1
