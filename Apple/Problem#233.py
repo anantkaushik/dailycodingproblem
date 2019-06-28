d96ec093da7e1725b8182ec212a8368ef6e954a3
anantkaushik <anant.kaushik2@gmail.com>
2019-06-29 01:30:17 +0530

    Nth-Fibonacci Number

---
+++ Apple/Problem#233.py
@@ -0,0 +1,13 @@
+"""
+Implement the function fib(n), which returns the nth number in the Fibonacci sequence, using only O(1) space.
+"""
+def getNthFib(n):
+	if n == 1:
+		return 0
+	elif n == 2:
+		return 1
+	a,b = 0,1
+	for _ in range(3,n+1):
+		a,b = b,a + b
+	print(b)
+	return b
